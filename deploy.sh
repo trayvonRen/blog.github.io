9a474f707e184b6a0ed24fd3b93be811e3274b07
woyasuohen6 <1669506811@qq.com>
2020-10-29 19:25:39 +0800

    add note

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add .
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f origin git@github.com:woyasuohen6/algorithm.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git remote add origin root@123.57.137.244:/web/repo/blog.git
+git push -f origin  master
+
+cd -
