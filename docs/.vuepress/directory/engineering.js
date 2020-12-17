8f830d861fe9d6677d5899d0b2114e60b897d4a2
woyasuohen6 <1669506811@qq.com>
2020-12-17 21:14:59 +0800

    add note

--- docs/.vuepress/directory/engineering.js
+++ docs/.vuepress/directory/engineering.js
@@ -1,5 +1,12 @@
 module.exports = [
   {
+    title: 'GIT', // 必要的
+    path: '/engineering/git/', // 可选的, 应该是一个绝对路径
+    collapsable: true, // 可选的, 默认值是 true,
+    sidebarDepth: 2, // 可选的, 默认值是 1
+    children: [],
+  },
+  {
     title: 'Webpack', // 必要的
     path: '/engineering/webpack/', // 可选的, 应该是一个绝对路径
     collapsable: true, // 可选的, 默认值是 true,
